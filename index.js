440101498b4a74a6c3607da693539596e8894cd4
richardzhichaolv <rechardlv@163.com>
2023-06-12 16:27:02 +0800

    mac first try

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const serverHandler = require('./src/app')
 const server = http.createServer(serverHandler)
 
 // mac电脑ip地址：   192.168.0.100
-// 笔记本电脑ip地址：   192.168.0.103
+// 笔记本电脑ip地址：   192.168.0.103 ip随意分配
 
 server.listen(5050,() => {
     console.log('监听到5050端口')
